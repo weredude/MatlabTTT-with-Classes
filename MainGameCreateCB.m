62234f9857d95a8fab7aa3381f8f3f173c7ce721
weredude <63823877+weredude@users.noreply.github.com>
2022-12-02 21:27:40 -0800

    Add files via upload

--- MainGameCreateCB.m
+++ MainGameCreateCB.m
@@ -14,3 +14,8 @@ else
     player2 = GamePlayer(-1, 'O',true);
     currentPlayer = GamePlayer(player1.value, player1.letter, false);
 end
+
+set(findobj('tag', 'winsTextST'), 'string', num2str(wins));
+set(findobj('tag', 'lossesTextST'), 'string', num2str(losses));
+set(findobj('tag', 'tiesTextST'), 'string', num2str(ties));
+set(findobj('tag', 'modeTextST'), 'string', mode);
--- OnGameOver.m
+++ OnGameOver.m
@@ -7,5 +7,8 @@ function[] = OnGameOver(board, isTie, currentPlayer)
         set(findobj('tag', ['Slot', num2str(index)]), 'Enable', 'off');
     end
     RecordGame(board, isTie, currentPlayer);
+    set(findobj('tag', 'winsTextST'), 'string', num2str(board.wins));
+    set(findobj('tag', 'lossesTextST'), 'string', num2str(board.losses));
+    set(findobj('tag', 'tiesTextST'), 'string', num2str(board.ties));
     EndGame(board);
 end
